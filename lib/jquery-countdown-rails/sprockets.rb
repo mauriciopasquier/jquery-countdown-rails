13f668471edf4a3c38ca10d7d8c72c356c252e8f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-07-29 01:18:09 -0300

    Style: change double to single quotes

--- lib/jquery-countdown-rails/sprockets.rb
+++ lib/jquery-countdown-rails/sprockets.rb
@@ -2,5 +2,5 @@
 module JqueryCountdownRails
   root_dir = File.expand_path(File.dirname(File.dirname(File.dirname(__FILE__))))
 
-  Sprockets.paths << File.join(root_dir, "vendor", "assets")
+  Sprockets.paths << File.join(root_dir, 'vendor', 'assets')
 end
