cfabc63072b6119c69c106e7493f4e26ee52b708
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-07-29 01:22:14 -0300

    Coding style: remove blank line

--- lib/jquery-countdown-rails.rb
+++ lib/jquery-countdown-rails.rb
@@ -2,7 +2,6 @@ require 'jquery-countdown-rails/version'
 
 # Add jquery-countdown to the asset pipeline
 module JqueryCountdownRails
-
   if defined? ::Rails
     if ::Rails.version.to_s < '3.1'
       require 'jquery-countdown-rails/railtie'
